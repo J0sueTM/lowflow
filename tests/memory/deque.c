972914fecf0e51ff067211dac0c1861c2b71a638
J0sueTM <me@j0suetm.com>
2025-11-08 20:57:07 -0300

    ref: separate deque frankestein integration test into smaller directed tests

--- tests/memory/deque.c
+++ tests/memory/deque.c
@@ -178,11 +178,151 @@ static MunitResult test_pop_tail__single_elem(
   return MUNIT_OK;
 }
 
-// TODO: This test does too many stuff at the same time. We should
-// find what many things are being tested here and break them up into
-// smaller tests, with identifiable and understandable names in order
-// to improve readability and future testing.
-static MunitResult test_multiple_operations(
+static MunitResult test_deque_grows_when_tail_is_pushed_after_head_full(
+  const MunitParameter params[], void *data
+) {
+  (void)params; (void)data;
+
+  LF_Deque deque = {
+    .elem_size = sizeof(double),
+    .elem_alignment = alignof(double),
+    .elem_qtt_in_block = 3
+  };
+  lf_init_deque(&deque);
+
+  lf_alloc_deque_head_elem(&deque);
+  lf_alloc_deque_head_elem(&deque);
+  lf_alloc_deque_head_elem(&deque);
+
+  munit_assert_size(deque.arena.block_count, ==, 1);
+
+  lf_alloc_deque_tail_elem(&deque);
+
+  munit_assert_size(deque.arena.block_count, ==, 2);
+
+  return MUNIT_OK;
+}
+
+static MunitResult test_deque_frees_blocks_when_emptied(
+  const MunitParameter params[], void *data
+) {
+  (void)params; (void)data;
+
+  LF_Deque deque = {
+    .elem_size = sizeof(double),
+    .elem_alignment = alignof(double),
+    .elem_qtt_in_block = 3
+  };
+  lf_init_deque(&deque);
+
+  lf_alloc_deque_head_elem(&deque);
+  lf_alloc_deque_tail_elem(&deque);
+  lf_alloc_deque_tail_elem(&deque);
+
+  // Should have allocated 2 blocks.
+  munit_assert_size(deque.arena.block_count, ==, 2);
+
+  lf_pop_head_elem_from_deque(&deque);
+  lf_pop_head_elem_from_deque(&deque);
+  lf_pop_head_elem_from_deque(&deque);
+
+  // Should be reduced to one reusable block.
+  munit_assert_size(deque.arena.block_count, ==, 1);
+
+  return MUNIT_OK;
+}
+
+static MunitResult test_deque_reuses_existing_blocks(
+  const MunitParameter params[], void *data
+) {
+  (void)params; (void)data;
+
+  LF_Deque deque = {
+    .elem_size = sizeof(double),
+    .elem_alignment = alignof(double),
+    .elem_qtt_in_block = 3
+  };
+  lf_init_deque(&deque);
+
+  for (int i = 0; i < 6; i++) {
+    lf_alloc_deque_head_elem(&deque);
+  }
+  munit_assert_size(deque.arena.block_count, ==, 2);
+
+  while (deque.elem_count > 0) {
+    lf_pop_head_elem_from_deque(&deque);
+  }
+  munit_assert_size(deque.arena.block_count, ==, 1);
+
+  lf_alloc_deque_head_elem(&deque);
+  lf_alloc_deque_head_elem(&deque);
+  munit_assert_size(deque.arena.block_count, ==, 1);
+
+  return MUNIT_OK;
+}
+
+static MunitResult test_deque_mixed_operations_preserve_order(
+  const MunitParameter params[], void *data
+) {
+  (void)params; (void)data;
+
+  LF_Deque deque = {
+    .elem_size = sizeof(double),
+    .elem_alignment = alignof(double),
+    .elem_qtt_in_block = 3
+  };
+  lf_init_deque(&deque);
+
+  double *a = (double *)lf_alloc_deque_head_elem(&deque); *a = 7;
+  double *b = (double *)lf_alloc_deque_head_elem(&deque); *b = 11;
+  double *tail = (double *)lf_alloc_deque_tail_elem(&deque); *tail = 29;
+
+  double *pop1 = (double *)lf_pop_head_elem_from_deque(&deque);
+  double *pop2 = (double *)lf_pop_tail_elem_from_deque(&deque);
+
+  munit_assert_double(*pop1, ==, 11);
+  munit_assert_double(*pop2, ==, 29);
+  munit_assert_size(deque.elem_count, ==, 1);
+
+  return MUNIT_OK;
+}
+
+static MunitResult test_deque_complex_growth_and_release(
+  const MunitParameter params[], void *data
+) {
+  (void)params; (void)data;
+
+  LF_Deque deque = {
+    .elem_size = sizeof(double),
+    .elem_alignment = alignof(double),
+    .elem_qtt_in_block = 3
+  };
+  lf_init_deque(&deque);
+
+  // Fill up multiple times to trigger 3 blocks.
+  for (int i = 0; i < 9; i++) {
+    double *e = (double *)lf_alloc_deque_head_elem(&deque);
+    *e = (double)i;
+  }
+  munit_assert_size(deque.arena.block_count, ==, 3);
+
+  // Pop a few from head and tail.
+  lf_pop_head_elem_from_deque(&deque);
+  lf_pop_tail_elem_from_deque(&deque);
+
+  // Should still keep 3 blocks since middle one is active.
+  munit_assert_size(deque.arena.block_count, ==, 3);
+
+  // Pop until only one block is left.
+  while (deque.elem_count > 1) {
+    lf_pop_head_elem_from_deque(&deque);
+  }
+  munit_assert_size(deque.arena.block_count, ==, 1);
+
+  return MUNIT_OK;
+}
+
+static MunitResult test_integration(
   const MunitParameter params[],
   void *data
 ) {
@@ -310,7 +450,12 @@ static MunitTest deque_tests[] = {
   { "/pop/head/multiple_blocks", test_pop_head__multiple_blocks, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { "/pop/tail/single_elem", test_pop_tail__single_elem, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   { "/pop/tail/multiple_blocks", test_pop_tail__multiple_blocks, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-  { "/multiple_operations", test_multiple_operations, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { "/grow/tail_after_head_full", test_deque_grows_when_tail_is_pushed_after_head_full, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { "/free/blocks_when_emptied", test_deque_frees_blocks_when_emptied, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { "/reuse/blocks", test_deque_reuses_existing_blocks, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { "/mixed_operations/preserve_order", test_deque_mixed_operations_preserve_order, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { "/complex_growth/release", test_deque_complex_growth_and_release, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  { "/integration", test_integration, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
   {0},
 };
 
